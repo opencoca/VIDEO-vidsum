8901163bffaff24d8f002ebe6c23422ea2ec3488
OpenCo <admin@openco.ca>
2021-09-10 14:18:41 -0400

    Capitlize all sentances

--- code/punctuate_md.py
+++ code/punctuate_md.py
@@ -1,5 +1,10 @@
 # coding: utf-8
 import requests
+import re
+
+# Define regular expression to detect sentances
+p = re.compile(r'(?<=[\.\?!]\s)(\w+)')
+
 from pprint import pprint
 
 #set punctuation API url for use with requests
@@ -36,24 +41,35 @@ def punctuate(text, url= punctuate_api_endpoint ):
         punctuated_text = ".".join([text for text in  punctuated_text.split(".")[0:-1]])
         return {"punctuated_text": punctuated_text +"." , "fragment": fragment}
 
+def cap(match):
+    return(match.group().capitalize())
+
 def defragment_text(blocks):
     for item in range(len(blocks)):
         next_item = item+1
         merge(blocks[item], punctuate(blocks[item]['text']))
         if('fragment' in blocks[item] and next_item < len(blocks)):
             blocks[next_item]['text'] = "{} {}".format(blocks[item]['fragment'], blocks[next_item]['text'])
+        else:
+            blocks[item]['punctuated_text'] = p.sub(cap, blocks[item]['punctuated_text'])
         if next_item == len(blocks):
             break
 
+# Defragment and punctuate the blocks of text
 defragment_text(block_list)
+
+block_list[0] = {
+    'image': '',
+    'text': block_list[0]['text'],
+    'punctuated_text': p.sub(cap, block_list[0]['punctuated_text'][4:].capitalize())
+}
 
 f = open("1.final.en.md","w")
 f.writelines(["{}\n \n {}".format(block['image'], block['punctuated_text']) for block in block_list])
 f.close()
 
-#for num in nums
-#for i, j in zip(my_list,my_list[1:])
-# if i<image[:7] <j
+
+
 
 #markdown image format
 #![image title](/imagepath "image alt text")
